87d3b9c1acdb7e47bf55775fbb6ef8998587ff80
Balazs776 <136377358+Balazs776@users.noreply.github.com>
2026-05-21 11:13:24 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,26 +1,75 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 
 
 root = Tk(screenName="Amőba", baseName="Amőba", className='Tk', useTk=1)
 root.title("Amőba")
 frm = ttk.Frame(root, padding=15)
 ttk.Label(frm, text="Amőba.")
+jatekos = "X"
+stop = False
+def clicked(k, n):
+    global jatekos
+    if jatekos == "X" and statusz[k][n] == 0 and stop == False:
+        felulet[k][n].config(text = "X")
+        statusz[k][n] = "X"
+        jatekos = 'O'
+    
+    elif jatekos == "O" and statusz[k][n] == 0 and stop == False:
+        felulet[k][n].config(text = "O")
+        statusz[k][n] = "O"
+        jatekos = "X"
 
-canvas = Canvas(root, width=400, height=400)
-canvas.pack()
+    nyerte()
 
+def nyerte():
+    global stop
+    
+    i = 0
+    while i < 3:
+        if statusz[i][0] == statusz[i][1] == statusz[i][2] != 0:
+            stop = True
 
-canvas.create_line(0, 50, 200, 50)
-canvas.create_line(0, 100, 200, 100)
-canvas.create_line(0, 150, 200, 150)
-canvas.create_line(0, 50, 200, 50)
-canvas.create_line(50, 100, 200, 100)
-canvas.create_line(0, 50, 128, 50)
+            nyer = messagebox.showinfo("Nyertes", statusz[i][0] + "Nyert")
+            break
+        elif statusz [0][i] == statusz[1][i] == statusz[2][i] != 0:
+            stop = True
 
+            nyer = messagebox.showinfo("Nyertes", statusz[0][i] + "Nyert")
+            break
 
-def left_click(event):
-    print(f"Left click at ({event.x}, {event.y})")
-root.bind("<Button-1>", left_click)
-ttk.Button(frm, text="Kilépés", command=root.destroy)
+        elif statusz[0][0] == statusz[1][1] == statusz[2][2] !=0:
+            stop = True
+
+            nyer = messagebox.showinfo("Nyertes", statusz[0][0] + "Nyert")
+            break
+
+        elif statusz[0][2] == statusz[1][1] == statusz[2][0] !=0:
+            stop = True
+
+            nyer = messagebox.showinfo("Nyertes", statusz[0][2] + "Nyert")
+            break
+
+        elif statusz[0][0] and statusz[0][1] and statusz[0][2] and statusz[1][0] and statusz[1][1] and statusz[1][2] and statusz[2][0] and statusz[2][1] and statusz[2][2] != 0:
+            stop = True
+
+            nyer = messagebox.showinfo("Döntetlen", "Döntetlen")
+    i += 1
+
+root.resizable(0,0)
+
+felulet = [[0,0,0], [0,0,0], [0,0,0]]
+
+statusz = [[0,0,0], [0,0,0], [0,0,0]]
+i = 0
+j = 0
+while i < 3:
+    j = 0
+    while j < 3:
+        felulet[i][j] = Button(height=5, width=10, text="", font = ("Oswald","25"), command = lambda k = i, n = j : clicked(k, n))
+        felulet[i][j].grid(row = i, column = j)
+        j += 1
+    i += 1
 root.mainloop()
+
